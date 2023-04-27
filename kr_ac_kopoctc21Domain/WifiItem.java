b487c9d2b280bb39a21ff8460b12007b38ca27e1
DESKTOP-VUEA0AQ\kopo24 <lss2star@gmail.com>
2023-04-27 19:50:18 +0900

    domain

---
+++ kr_ac_kopoctc21Domain/WifiItem.java
@@ -0,0 +1,21 @@
+package kr_ac_kopoctc21Domain;
+
+public class WifiItem {
+	private int id;
+	private String installationLocationName;
+	private String installationLocationDetails;
+	private String installationCityName;
+	private String installationDistrictName;
+	private String installationFacilityType;
+	private String serviceProviderName;
+	private String wifiSsid;
+	private String dateOfInstallation;
+	private String roadNameAddress;
+	private String lotNumberAddress;
+	private String managementNgencyName;
+	private String managementAgencyPhoneNumber;
+	private double latitude;
+	private double longitude;
+	private String created;
+
+}
