e42358ffa6466867a6568964e6630223c2ae1670
DESKTOP-VUEA0AQ\kopo24 <lss2star@gmail.com>
2023-04-27 19:50:03 +0900

    dao폴더

---
+++ kr_ac_kopoctc21Dao/WifiItemDao.java
@@ -0,0 +1,19 @@
+package kr_ac_kopoctc21Dao;
+
+public class WifiItemDao {
+	public WifiItem create (WifiItem wifiItem) {
+		return null;
+	}
+	public WifiItem selectOne (int id) {
+		return null;
+	}
+	public List<WifiItem> selectAll () {
+		return null;
+	}
+	public WifiItem update (WifiItem wifiItem) {
+		return null;
+	}
+	public WifiItem delete (int id) {
+		return null;
+	}
+}
