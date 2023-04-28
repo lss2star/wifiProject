61a4fb1a832259c2ac0e0ddf3131c385a98045f4
DESKTOP-VUEA0AQ\kopo24 <lss2star@gmail.com>
2023-04-28 16:20:24 +0900

    두 사이의 거리 계산 식 완료

--- kr_ac_kopoctc21Service/WifiItemService.java
+++ kr_ac_kopoctc21Service/WifiItemService.java
@@ -1,7 +1,16 @@
 package kr_ac_kopoctc21Service;
 
+import kr_ac_kopoctc21Domain.WifiItem;
+
 public class WifiItemService {
-	public double getDistance (WifiItem wifiItem1, WifiItem wifiItem2) {
-		return 0;
+	public double getDistance (WifiItem me, WifiItem target) {
+		double lat1 = me.getLatitude();
+	    double lng1 = me.getLongitude();
+	    double lat2 = target.getLatitude();
+	    double lng2 = target.getLongitude();
+
+	    double dist = Math.sqrt(Math.pow(lat1 - lat2, 2) + Math.pow(lng1 - lng2, 2));
+	    return dist;
 	}
+	
 }
